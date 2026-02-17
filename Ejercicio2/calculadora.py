54a9b38177749448b88d51a736f9665c9ef6a7e9
Alexis Reyes <alexisreyess078@gmail.com>
2026-02-17 16:42:27 +0100

    Agregada imagen del ejercicio

---
+++ Ejercicio2/calculadora.py
@@ -0,0 +1,21 @@
+numero = int(input("Ingresa un número decimal: "))
+
+def convertir(numero, base):
+    if numero == 0:
+        return "0"
+    
+    digitos = "0123456789ABCDEF"
+    resultado = ""
+    
+    while numero > 0:
+        residuo = numero % base
+        resultado = digitos[residuo] + resultado
+        numero = numero // base
+    
+    return resultado
+
+
+print("\nResultados:")
+print("Binario:", convertir(numero, 2))
+print("Octal:", convertir(numero, 8))
+print("Hexadecimal:", convertir(numero, 16))
